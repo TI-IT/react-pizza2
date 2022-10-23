daff2710928c5c55d1de5d94761f841d37bf5f07
TI-IT <go.tiit.ogle@gmail.com>
2022-10-23 16:44:58 +0300

    TC 0.2

---
+++ .history/src/components/Categories_20221023155317.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Categories: React.FC = ({ value, onChangeCategory }) => {
+  const categories = ['все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((categoryName, i) => (
+          <li key={i} onClick={() => onChangeCategory(i)} className={value === i ? 'active' : ''}>
+            {categoryName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+export default Categories
